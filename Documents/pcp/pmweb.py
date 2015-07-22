7b7c6528a26f3214d07f985a251a829a118538c0
Gauri Dasgupta <gdasgupt@dhcp-16-101.sjc.redhat.com>
2015-07-22 11:27:07 -0700

    added

---
+++ Documents/pcp/pmweb.py
@@ -0,0 +1,34 @@
+import urllib2
+import json
+
+class pmweb:
+	def __init__(self, host, port, server):
+		self.host = host
+		self.port = port
+		self.server = server
+
+	def getContext(self):
+		url = "http://" + self.host + ":" + str(self.port) + "/pmapi/context?" + "hostname=" + self.server
+		print  url
+		response = urllib2.urlopen(url)
+		res = response.read()
+		res = json.loads(res)
+		return res["context"]
+	
+	def getStats(self, context, stats):
+		url = "http://" + self.host + ":" + str(self.port) + "/pmapi/" + context + "/_fetch?names=" + stats 
+		print  url
+		response = urllib2.urlopen(url)
+		res = response.read()
+		res = json.loads(res)
+		return res
+		pass
+
+
+	def getName(self, context, name):
+		url = "http://" + self.host + ":" + str(self.port) + "/pmapi/" + context + "/_indom?name=" + name + "&instance=" + "3"
+		print  url
+		response = urllib2.urlopen(url)
+		res = response.read()
+		res = json.loads(res)
+		return res
